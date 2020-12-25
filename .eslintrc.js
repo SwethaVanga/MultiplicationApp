9113e1eb86d36a51e364786bb61540907cfde0ad
Swetha Sangam <swetha.sangam@gmail.com>
2020-12-25 13:55:10 +0000

    Initial commit - dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  'env': {
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-var': 1,
+    'eqeqeq': 1
+  }
+}
