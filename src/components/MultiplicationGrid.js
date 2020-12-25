af95568333867ad7137d90b1cdd950703d704adb
Swetha Sangam <swetha.sangam@gmail.com>
2020-12-25 15:37:15 +0000

    Styles and updates

---
+++ src/components/MultiplicationGrid.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Container, Row} from 'react-bootstrap'
+import { MAX_NUMBER } from '../utils/config'
+import { getNumbers, getMultiplesFromRange } from '../utils/numbers'
+
+const MultiplicationGrid = () => {
+  let [multiples, setMultiples] = useState([])
+  const numbers = getNumbers(MAX_NUMBER)
+
+  /**
+	 * The multiples of current number been calculated and set and highlighted on click event
+	*/
+  const handleClickNumber = (number) => {
+    const multiples = getMultiplesFromRange(number, numbers)
+    setMultiples(multiples)
+  }
+
+  return (
+    <Container>
+      <h1 className='container__heading'>
+				Multiplication app
+      </h1>
+      <Row className='container__table'>
+        {numbers.map((number, i) => (
+          <button
+            key={i}
+            className={`table__item ${multiples.includes(number) ? 'table__item--highlight' : ''}`}
+            aria-pressed={multiples.includes(number)}
+            onClick={() => handleClickNumber(number)}>{number}
+          </button>
+        ))}
+      </Row>
+    </Container>
+  )
+}
+
+export default MultiplicationGrid
